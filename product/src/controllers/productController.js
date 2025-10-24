3bdeaa6867b21b37403859bb50db3fb5a9bf6efb
HaiDang102 <haidang23102003@gmail.com>
2025-10-24 20:01:28 +0700

    update code

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -10,7 +10,7 @@ class ProductController {
     constructor() {
         this.createOrder = this.createOrder.bind(this);
         this.getOrderStatus = this.getOrderStatus.bind(this);
-        // this.getProductById = this.getProductById.bind(this);
+        this.getProductById = this.getProductById.bind(this);
         this.ordersMap = new Map();
 
     }
@@ -120,6 +120,15 @@ class ProductController {
         //     }
         //     //-----------------------------------------------------------//
 
+    async getProductById(req, res) {
+        try {
+            const product = await Product.findById(req.params.id);
+            if (!product) return res.status(404).json({ message: "Product not found" });
+            res.json(product);
+        } catch (error) {
+            res.status(500).json({ message: "Server error" });
 
+        }
+    }
 }
 module.exports = ProductController;
